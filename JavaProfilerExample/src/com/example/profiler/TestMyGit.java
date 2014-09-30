e7050af4d54c0bf611ba6bdff0ea22e5cdacfbba
freakD <sarjanshrestha@hotmail.com>
2014-09-30 14:32:07 -0400

    new commit

--- JavaProfilerExample/src/com/example/profiler/TestMyGit.java
+++ JavaProfilerExample/src/com/example/profiler/TestMyGit.java
@@ -4,6 +4,7 @@ public class TestMyGit {
 	public static void main(String[] args) {
 		System.out.println("Calling GIT account.");
 		System.out.println("Hello All");
+		System.out.println("Check for new changes.");
 	}
 
 }
